be2042377029d242ef700c7725bd3395ea2f8b1e
ZoeZo3 <106765845+ZoeZo3@users.noreply.github.com>
2023-11-02 16:56:06 +0100

    Carousel on homepage
    - integrate carousel on HP
    - update search bar

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 #How to run the code?
 
+- go to the library-app folder
 - install node js
+- run: npm i && npm run dev
--- library-app/src/pages/Homepage.jsx
+++ library-app/src/pages/Homepage.jsx
@@ -1,41 +1,67 @@
-import React from "react";
-import { useLoaderData, Link } from "react-router-dom"
+import React, { Suspense, useState } from "react";
+import { useLoaderData, Link, Await } from "react-router-dom"
 import { getBooks } from "../../utils";
 
 export async function loader() {
-    return await getBooks(undefined, "newest")
+    return {books: await getBooks(undefined)} //, "newest"
 }
 
 export default function Homepage() {
-    const books = useLoaderData()
-    console.log(books)
+    const dataPromise = useLoaderData()    
 
-    const bookElements = books.map(book => {
+    function renderBooks(books) {
+        const books_filtered = books.filter(book =>
+            book.volumeInfo.imageLinks && book.volumeInfo.authors
+            )
+
+        const [currentIndex, setCurrentIndex] = useState(0);
+        
+        const handleNextClick = () => {
+            if (currentIndex < books_filtered.length - 3) {
+                setCurrentIndex(index => index + 1)
+            }
+        }
+
+        const handlePrevClick = () => {
+            if (currentIndex >= 1) {
+                setCurrentIndex(index => index - 1)
+            }
+        }
+    
+        const bookElements = books_filtered.slice(currentIndex, currentIndex + 3).map((book, index) => {
+            return (
+                <Link className="book-card carousel-item" key={index} to={`books/${book.id}`}>
+                    <div className="book-card-img">
+                        <img src={book.volumeInfo.imageLinks.thumbnail} />
+                    </div>
+                    <p><b>{book.volumeInfo.title}</b></p>
+                    <p><small>{book.volumeInfo.authors.join(', ')}</small></p>
+                </Link>
+            )
+        })
+
         return (
-            book.volumeInfo.imageLinks && book.volumeInfo.description && book.volumeInfo.authors &&
-            <Link className="book-card" key={book.id} to={book.id}>
-                <div className="book-card-img">
-                    <img src={book.volumeInfo.imageLinks.thumbnail} />
+            <div className="carousel">
+                <button className="carousel-button" onClick={handlePrevClick} disabled={currentIndex === 0}>
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                <div className="carousel-items">
+                    {bookElements}
                 </div>
-                <p><b>{book.volumeInfo.title}</b></p>
-                <p><small>{book.volumeInfo.authors.join(', ')}</small></p>
-            </Link>
-        )
-    })
+                <button className="carousel-button" onClick={handleNextClick} disabled={currentIndex === books_filtered.length - 3}>
+                    <i className="fas fa-chevron-right"></i>
+                </button>
+            </div>
+    )}
+
 
     return (
         <div>
             <h1>Newest books</h1>
-            <div className="books-listing">
-                {bookElements}
-            </div>
+            <Suspense fallback={<h2 style={{ display: 'flex', marginTop: '60px'}}>Loading books...</h2>}>
+                <Await resolve={dataPromise.books}>
+                    {renderBooks}
+                </Await>
+            </Suspense>  
         </div>
     )}
-
-    /*return (
-        <Suspense fallback={<h2 style={{ display: 'flex', marginTop: '60px'}}>Loading books...</h2>}>
-            <Await resolve={dataPromise.books}>
-                    {renderBooks}
-            </Await>
-        </Suspense>
-    )*/
